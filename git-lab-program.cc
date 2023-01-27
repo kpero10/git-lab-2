57767938a36ace2e1fd83e0b6ee380c957569c90
Kyle <kp012821@ohio.edu>
2023-01-27 15:14:01 -0500

    code for git lab

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Kyle Pero
+ *        Date: 01/27/2023
+ * Description: code for git lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
